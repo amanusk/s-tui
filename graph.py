1bc5b1fdba53ba98751df3b100c0cd9851016842
giltsuker <giltsuker@yahoo.com>
2017-04-10 12:05:42 +0300

    - changed mode names - PEP8 approved! (indednt, spaced, etc...)

--- graph.py
+++ graph.py
@@ -34,12 +34,14 @@ import time
 
 UPDATE_INTERVAL = 1
 
-def sin100( x ):
+
+def sin100(x):
     """
     A sin function that returns values between 0 and 100 and repeats
     after x == 100.
     """
-    return 50 + 50 * math.sin( x * math.pi / 50 )
+    return 50 + 50 * math.sin(x * math.pi / 50)
+
 
 class GraphModel:
     """
@@ -50,9 +52,9 @@ class GraphModel:
     data_max_value = 100
 
     def __init__(self):
-        data = [ ('Saw', range(0,100,2)*2),
-            ('Rand', [random.randint(0, 10)
-                        for x in range(100)]),
+        data = [
+            ('Regular Operation', [0] * 100),
+            ('Stress Operation', [0] * 100),
             ]
         self.modes = []
         self.data = {}
@@ -60,11 +62,14 @@ class GraphModel:
             self.modes.append(m)
             self.data[m] = d
 
+        self.current_mode = self.modes[0]
+
     def get_modes(self):
         return self.modes
 
     def set_mode(self, m):
         self.current_mode = m
+        return True
 
     def get_data(self, offset, r):
         """
@@ -90,24 +95,24 @@ class GraphView(urwid.WidgetWrap):
     """
 
     palette = [
-        ('body',         'black',      'light gray', 'standout'),
-        ('header',       'white',      'dark red',   'bold'),
-        ('screen edge',  'light blue', 'dark cyan'),
-        ('main shadow',  'dark gray',  'black'),
-        ('line',         'black',      'light gray', 'standout'),
-        ('bg background','light gray', 'black'),
-        #('bg 1',         'black',      'dark blue', 'standout'),
-        ('bg 1',         'black',      'dark green', 'standout'),
-        ('bg 1 smooth',  'dark blue',  'black'),
-        #('bg 2',         'black',      'dark cyan', 'standout'),
-        ('bg 2',         'dark red',    'light green', 'standout'),
-        ('bg 2 smooth',  'dark cyan',  'black'),
-        ('button normal','light gray', 'dark blue', 'standout'),
-        ('button select','white',      'dark green'),
-        ('line',         'black',      'light gray', 'standout'),
-        ('pg normal',    'white',      'black', 'standout'),
-        ('pg complete',  'white',      'dark magenta'),
-        ('pg smooth',     'dark magenta','black')
+        ('body',          'black',      'light gray', 'standout'),
+        ('header',        'white',      'dark red',   'bold'),
+        ('screen edge',   'light blue', 'dark cyan'),
+        ('main shadow',   'dark gray',  'black'),
+        ('line',          'black',      'light gray', 'standout'),
+        ('bg background', 'light gray', 'black'),
+        # ('bg 1',         'black',      'dark blue', 'standout'),
+        ('bg 1',          'black',      'dark green', 'standout'),
+        ('bg 1 smooth',   'dark blue',  'black'),
+        # ('bg 2',         'black',      'dark cyan', 'standout'),
+        ('bg 2',          'dark red',    'light green', 'standout'),
+        ('bg 2 smooth',   'dark cyan',  'black'),
+        ('button normal', 'light gray', 'dark blue', 'standout'),
+        ('button select', 'white',      'dark green'),
+        ('line',          'black',      'light gray', 'standout'),
+        ('pg normal',     'white',      'black', 'standout'),
+        ('pg complete',   'white',      'dark magenta'),
+        ('pg smooth',     'dark magenta', 'black')
         ]
 
     graph_samples_per_bar = 10
@@ -123,6 +128,11 @@ class GraphView(urwid.WidgetWrap):
         self.last_offset = None
         self.cpu_util = [0] * self.graph_num_bars
         self.cpu_temp = [0] * self.graph_num_bars
+        self.mode_buttons = []
+        self.animate_button = []
+        self.graph_util = []
+        self.animate_progress = []
+        self.animate_progress_wrap = []
 
         urwid.WidgetWrap.__init__(self, self.main_window())
 
@@ -135,14 +145,14 @@ class GraphView(urwid.WidgetWrap):
         return int(self.offset + (tdelta*self.graph_offset_per_second))
 
     def update_util(self):
-        #last_value = random.randint(0,100)
+        # last_value = random.randint(0,100)
         last_value = psutil.cpu_percent(interval=None)
         self.cpu_util.append(last_value)
         self.cpu_util = self.cpu_util[1:]
 
     def update_temp(self):
-        temps = psutil.sensors_temperatures()
-        #TODO make this more robust
+        # temps = psutil.sensors_temperatures()  # TODO is this needed?
+        # TODO make this more robust
         last_value = psutil.sensors_temperatures()['acpitz'][0].current
         self.cpu_temp.append(last_value)
         self.cpu_temp = self.cpu_temp[1:]
@@ -154,7 +164,7 @@ class GraphView(urwid.WidgetWrap):
         self.last_offset = o
         gspb = self.graph_samples_per_bar
         r = gspb * self.graph_num_bars
-        d, max_value, repeat = self.controller.get_data( o, r )
+        d, max_value, repeat = self.controller.get_data(o, r)
         l = []
         self.update_util()
         self.update_temp()
@@ -164,10 +174,10 @@ class GraphView(urwid.WidgetWrap):
             value = self.cpu_util[n]
             # toggle between two bar types
             if n & 1:
-                l.append([0,value])
+                l.append([0, value])
             else:
-                l.append([value,0])
-        self.graph_util.set_data(l,max_value)
+                l.append([value, 0])
+        self.graph_util.set_data(l, max_value)
 
         # Updating CPU temperature
         l = []
@@ -175,26 +185,26 @@ class GraphView(urwid.WidgetWrap):
             value = self.cpu_temp[n]
             # toggle between two bar types
             if n & 1:
-                l.append([0,value])
+                l.append([0, value])
             else:
-                l.append([value,0])
-        self.graph_temp.set_data(l,max_value)
+                l.append([value, 0])
+        self.graph_temp.set_data(l, max_value)
 
         # also update progress
-        #if (o//repeat)&1:
+        # if (o//repeat)&1:
         #    # show 100% for first half, 0 for second half
         #    if o%repeat > repeat//2:
         #        prog = 0
         #    else:
         #        prog = 1
-        #else:
+        # else:
         #    prog = float(o%repeat) / repeat
-        #self.animate_progress.set_completion( prog )
-        #return True
+        # self.animate_progress.set_completion( prog )
+        # return True
 
     def on_animate_button(self, button):
         """Toggle started state and button text."""
-        if self.started: # stop animation
+        if self.started:  # stop animation
             button.set_label("Start")
             self.offset = self.get_offset_now()
             self.started = False
@@ -205,7 +215,6 @@ class GraphView(urwid.WidgetWrap):
             self.start_time = time.time()
             self.controller.animate_graph()
 
-
     def on_reset_button(self, w):
         self.offset = 0
         self.start_time = time.time()
@@ -215,7 +224,7 @@ class GraphView(urwid.WidgetWrap):
         """Notify the controller of a new mode setting."""
         if state:
             # The new mode is the label of the button
-            self.controller.set_mode( button.get_label() )
+            self.controller.set_mode(button.get_label())
         self.last_offset = None
 
     def on_mode_change(self, m):
@@ -227,31 +236,30 @@ class GraphView(urwid.WidgetWrap):
         self.last_offset = None
 
     def on_unicode_checkbox(self, w, state):
-        self.graph_util = self.bar_graph( state )
+        self.graph_util = self.bar_graph(state)
         self.graph_wrap._w = self.graph_util
-        self.animate_progress = self.progress_bar( state )
+        self.animate_progress = self.progress_bar(state)
         self.animate_progress_wrap._w = self.animate_progress
-        self.update_graph( True )
-
+        self.update_graph(True)
 
     def main_shadow(self, w):
         """Wrap a shadow and background around widget w."""
         bg = urwid.AttrWrap(urwid.SolidFill(u"\u2592"), 'screen edge')
         shadow = urwid.AttrWrap(urwid.SolidFill(u" "), 'main shadow')
 
-        bg = urwid.Overlay( shadow, bg,
-            ('fixed left', 3), ('fixed right', 1),
-            ('fixed top', 2), ('fixed bottom', 1))
-        w = urwid.Overlay( w, bg,
-            ('fixed left', 2), ('fixed right', 3),
-            ('fixed top', 1), ('fixed bottom', 2))
+        bg = urwid.Overlay(shadow, bg,
+                           ('fixed left', 3), ('fixed right', 1),
+                           ('fixed top', 2), ('fixed bottom', 1))
+        w = urwid.Overlay(w, bg,
+                          ('fixed left', 2), ('fixed right', 3),
+                          ('fixed top', 1), ('fixed bottom', 2))
         return w
 
     def bar_graph(self, smooth=False):
         satt = None
         if smooth:
-            satt = {(1,0): 'bg 1 smooth', (2,0): 'bg 2 smooth'}
-        w = urwid.BarGraph(['bg background','bg 1','bg 2'], satt=satt)
+            satt = {(1, 0): 'bg 1 smooth', (2, 0): 'bg 2 smooth'}
+        w = urwid.BarGraph(['bg background', 'bg 1', 'bg 2'], satt=satt)
         return w
 
     def button(self, t, fn):
@@ -267,10 +275,10 @@ class GraphView(urwid.WidgetWrap):
     def progress_bar(self, smooth=False):
         if smooth:
             return urwid.ProgressBar('pg normal', 'pg complete',
-                0, 1, 'pg smooth')
+                                     0, 1, 'pg smooth')
         else:
             return urwid.ProgressBar('pg normal', 'pg complete',
-                0, 1)
+                                     0, 1)
 
     def exit_program(self, w):
         raise urwid.ExitMainLoop()
@@ -278,20 +286,19 @@ class GraphView(urwid.WidgetWrap):
     def graph_controls(self):
         modes = self.controller.get_modes()
         # setup mode radio buttons
-        self.mode_buttons = []
         group = []
         for m in modes:
-            rb = self.radio_button( group, m, self.on_mode_button )
-            self.mode_buttons.append( rb )
+            rb = self.radio_button(group, m, self.on_mode_button)
+            self.mode_buttons.append(rb)
         # setup animate button
-        self.animate_button = self.button( "", self.on_animate_button)
-        self.on_animate_button( self.animate_button )
+        self.animate_button = self.button("", self.on_animate_button)
+        self.on_animate_button(self.animate_button)
         self.offset = 0
         self.animate_progress = self.progress_bar()
-        animate_controls = urwid.GridFlow( [
+        animate_controls = urwid.GridFlow([
             self.animate_button,
             self.button("Reset", self.on_reset_button),
-            ], 9, 2, 0, 'center')
+        ], 9, 2, 0, 'center')
 
         if urwid.get_encoding_mode() == "utf8":
             unicode_checkbox = urwid.CheckBox(
@@ -304,16 +311,16 @@ class GraphView(urwid.WidgetWrap):
         self.animate_progress_wrap = urwid.WidgetWrap(
             self.animate_progress)
 
-        l = [    urwid.Text("Mode",align="center"),
-            ] + self.mode_buttons + [
+        l = [urwid.Text("Mode", align="center"),
+             ] + self.mode_buttons + [
             urwid.Divider(),
-            urwid.Text("Animation",align="center"),
+            urwid.Text("Animation", align="center"),
             animate_controls,
             self.animate_progress_wrap,
             urwid.Divider(),
-            urwid.LineBox( unicode_checkbox ),
+            urwid.LineBox(unicode_checkbox),
             urwid.Divider(),
-            self.button("Quit", self.exit_program ),
+            self.button("Quit", self.exit_program),
             ]
         w = urwid.ListBox(urwid.SimpleListWalker(l))
         return w
@@ -324,22 +331,24 @@ class GraphView(urwid.WidgetWrap):
         # TODO: Optional if graph could be stretched
         self.graph_util.set_bar_width(1)
         self.graph_temp.set_bar_width(1)
-        #self.graph_wrap = urwid.WidgetWrap( self.graph_util )
-        vline = urwid.AttrWrap( urwid.SolidFill(u'\u2502'), 'line')
+        # self.graph_wrap = urwid.WidgetWrap( self.graph_util )
+        vline = urwid.AttrWrap(urwid.SolidFill(u'\u2502'), 'line')
         hline = urwid.AttrWrap(urwid.SolidFill(u'\N{LOWER ONE QUARTER BLOCK}'), 'line')
 
         c = self.graph_controls()
-        l = [('weight',2,self.graph_util), ('fixed',1,hline), ('weight',2,self.graph_temp)]
+        l = [('weight', 2, self.graph_util),
+             ('fixed',  1, hline),
+             ('weight', 2, self.graph_temp)]
 
         r = urwid.Pile(l, focus_item=None)
 
-        w = urwid.Columns([('weight',2,r),
-            ('fixed',1,vline), c],
-            dividechars=1, focus_column=2)
-        w = urwid.Padding(w,('fixed left',1),('fixed right',0))
-        w = urwid.AttrWrap(w,'body')
+        w = urwid.Columns([('weight', 2, r),
+                           ('fixed', 1, vline), c],
+                          dividechars=1, focus_column=2)
+        w = urwid.Padding(w, ('fixed left', 1), ('fixed right', 0))
+        w = urwid.AttrWrap(w, 'body')
         w = urwid.LineBox(w)
-        w = urwid.AttrWrap(w,'line')
+        w = urwid.AttrWrap(w, 'line')
         w = self.main_shadow(w)
         return w
 
@@ -350,14 +359,15 @@ class GraphController:
     the application.
     """
     def __init__(self):
+        self.loop = []
         self.animate_alarm = None
         self.model = GraphModel()
-        self.view = GraphView( self )
+        self.view = GraphView(self)
         # use the first mode as the default
         mode = self.get_modes()[0]
-        self.model.set_mode( mode )
+        self.model.set_mode(mode)
         # update the view
-        self.view.on_mode_change( mode )
+        self.view.on_mode_change(mode)
         self.view.update_graph(True)
 
     def get_modes(self):
@@ -366,14 +376,13 @@ class GraphController:
 
     def set_mode(self, m):
         """Allow our view to set the mode."""
-        rval = self.model.set_mode( m )
+        rval = self.model.set_mode(m)
         self.view.update_graph(True)
         return rval
 
     def get_data(self, offset, range):
         """Provide data to our view for the graph."""
-        return self.model.get_data( offset, range )
-
+        return self.model.get_data(offset, range)
 
     def main(self):
         self.loop = urwid.MainLoop(self.view, self.view.palette)
@@ -395,5 +404,5 @@ class GraphController:
 def main():
     GraphController().main()
 
-if '__main__'==__name__:
+if '__main__' == __name__:
     main()
