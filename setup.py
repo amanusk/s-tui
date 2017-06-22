83b316e31b980f874a6f81d89d013c0a2de113be
amanusk <alex.manuskin@gmail.com>
2017-06-22 09:57:54 +0300

    Update setup.py for PyPi uploads

--- setup.py
+++ setup.py
@@ -3,18 +3,26 @@ from setuptools import setup
 
 setup(
     name = "s-tui",
-    version = "0.2.1",
+    packages=['s_tui'],
+    version = "0.2.2",
     author = "Alex Manuskin",
     author_email = "alex.manuskin@gmail.com",
     description = "Terminal UI stress test and monitoring tool",
     license = "GPL2",
-    url = "https://amanusk.github.io/s-tui/",
-    packages=['s_tui'],
+    url = "https://github.com/amanusk/s-tui",
+    download_url = 'https://github.com/amanusk/s-tui/archive/v0.2.1.tar.gz',
+    keywords = ['stress', 'monitoring', 'TUI'], # arbitrary keywords
+
     entry_points = {
         'console_scripts' : ['s-tui=s_tui.s_tui:main']
     },
     classifiers=[
-        "License :: OSI Approved :: GPL2 License",
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2',
+        'Topic :: System :: Monitoring',
     ],
-    install_requires=[ 'urwid', 'psutil', ],
+    install_requires=[
+        'urwid',
+        'psutil',
+    ],
 )
