1cb3975da029caa65d1b5beb807fd3a568c8b47a
Unknown <smaorus@gmail.com>
2017-08-21 13:48:36 +0300

    Added reading max power consumption file

--- s_tui/RaplPower.py
+++ s_tui/RaplPower.py
@@ -10,22 +10,36 @@ class RaplPower:
     def __init__(self, package_number = 0):
         self.package_number = package_number
         self.intel_rapl_package_energy_file = os.path.join(self.intel_rapl_folder, 'intel-rapl:%d'%package_number, 'energy_uj')
-        if (not os.path.exists(self.intel_rapl_package_energy_file)):
+        self.intel_rapl_package_max_energy_file = os.path.join(self.intel_rapl_folder, 'intel-rapl:%d'%package_number, 'constraint_0_max_power_uw')
+        if (not os.path.exists(self.intel_rapl_package_energy_file) or not os.path.exists(self.intel_rapl_package_max_energy_file)):
             self.is_available = False
+            self.last_measurement_time = 0
+            self.last_measurement_value = 0
+            self.max_power = 0
             return
 
         self.is_available = True
+        
         self.last_measurement_time = time.time()
         self.last_measurement_value = self.read_power_measurement_file()
+        self.max_power = read_max_power_file()
+
+
+    def read_measurement(file_path):
+        file = open(file_path)
+        value = file.read()
+        file.close()
+        return float(value)
+
+    def read_max_power_file(self):
+        if not self.is_available:
+            return -1
+        return float(read_measurement(self.intel_rapl_package_max_energy_file))   
 
     def read_power_measurement_file(self):
         if not self.is_available:
             return -1
-        file = open(self.intel_rapl_package_energy_file)
-        current_measurement_value = file.read()
-        file.close()
-        return float(current_measurement_value)
-
+        return float(read_measurement(self.intel_rapl_package_energy_file))   
 
     def get_power_usage(self):
         if not self.is_available:
