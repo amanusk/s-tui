7f93f5fa8d6af886effa19cb1797bd42bc13db4d
Unknown <smaorus@gmail.com>
2017-08-22 17:48:57 +0300

    Added basic files for the new source and graph classes

---
+++ s_tui/Graph/StuiBarGraph.py
@@ -0,0 +1,9 @@
+from ComplexBarGraphs import LabeledBarGraph
+from ComplexBarGraphs import ScalableBarGraph
+
+class StuiBarGraph:
+    def __init__(self, source, title, measurement_unit):
+        self.source = source
+        self.title = title
+        self.measurement_unit = measurement_unit
+
---
+++ s_tui/Sources/Source.py
@@ -0,0 +1,9 @@
+
+
+class Source:
+
+    def get_reading(self):
+        raise NotImplementedError("Get reading is not implemented")
+
+    def get_maximum(self):
+        raise NotImplementedError("Get maximum is not implemented")
