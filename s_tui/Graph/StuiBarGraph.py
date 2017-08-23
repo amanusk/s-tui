6ad9477a4556d699afb23747f674377569189d3f
Unknown <smaorus@gmail.com>
2017-08-23 11:45:44 +0300

    Added basic scalable graph instantiation

--- s_tui/Graph/StuiBarGraph.py
+++ s_tui/Graph/StuiBarGraph.py
@@ -1,9 +1,13 @@
 from ComplexBarGraphs import LabeledBarGraph
 from ComplexBarGraphs import ScalableBarGraph
 
-class StuiBarGraph:
-    def __init__(self, source, title, measurement_unit):
+class StuiBarGraph(LabeledBarGraph):
+    def __init__(self, source, title, measurement_unit, color_a, color_b, x_label, y_label):
         self.source = source
         self.title = title
         self.measurement_unit = measurement_unit
+
+
+        w = ScalableBarGraph(['bg background', color_a, color_b])
+        super(StuiBarGraph, self).__init__([w, x_label, y_label, title])
 
