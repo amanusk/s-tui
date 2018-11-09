3318c3ce5b5d1e6c1788c60d84467ad0b6f566e1
giltsuker <giltsuker@yahoo.com>
2018-11-09 21:49:08 +0100

    - fixed sensor menu so changing more than one sensor type will update both graph vectors

--- s_tui/SensorsMenu.py
+++ s_tui/SensorsMenu.py
@@ -119,8 +119,5 @@ class SensorsMenu:
 
             self.sensor_current_active_dict[s_name] = cb_sensor_visibility
 
-            if update_sensor_visibility:
-                break
-
         self.set_checkbox_value()
         self.return_fn(update=update_sensor_visibility)
