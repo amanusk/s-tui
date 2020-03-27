1a50005786a9c06cb04a8526aafffb5be03c96c1
Justas Zabulionis <38244149+Justazzz@users.noreply.github.com>
2020-03-27 09:12:24 +0100

    update copyright date to 2020

--- s_tui/sources/rapl_read.py
+++ s_tui/sources/rapl_read.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2017-2019 Alex Manuskin, Maor Veitsman
+# Copyright (C) 2017-2020 Alex Manuskin, Maor Veitsman
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
